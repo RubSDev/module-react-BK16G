787819931009b943a82f9d900dec4a147ccf6b83
RubS <rub.somuano.g@gmail.com>
2022-03-10 22:07:53 -0600

    Fix the season

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Button({ children, color }) {
-  return (
-    <button style={{ background: color, color: "white" }}>{children}</button>
-  );
-}
-
-export default Button;
--- src/components/Final.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function FinalHeader(props) {
-  const { name } = props;
-  return <header>{name}</header>;
-}
-
-export default FinalHeader;
