11a439c121ddfc89605ba6b998f4e1205464e288
RubS <rub.somuano.g@gmail.com>
2022-03-11 11:11:21 -0600

    Hola mundo con react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello world from react !</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/RubSDev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Soy RubS Github
         </a>
+        <br />
+        <footer>Kodemia ©2022 hecho con amor por RubS </footer>
       </header>
     </div>
   );
