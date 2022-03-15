39bc7d7ce412669843f0e876ddf74834a6d8892d
RubS <rub.somuano.g@gmail.com>
2022-03-14 21:35:04 -0600

    ...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ fuction Home( ){
+   return(
+     <div id='App'>
+       <header>Logo Links</header>
+       <div id='Saludo'>
+         <p>Saludo</p>
+         <p>What are you</p>
+       </div>
+       <div id='producyos'>
+         <p> card</p>
+
+       </div>
+       </div>
+   )
+ }
+
+ export default Home
